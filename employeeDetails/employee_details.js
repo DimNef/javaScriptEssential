796db78bf99590c9eea7f5834e50c9ee0f1efc64
DimNef <dimkrakis@gmail.com>
2024-10-16 15:50:22 -0400

    Arrays Function second push Nef in charge

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,8 +1,8 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-      { id: 4, name: 'Nefeli Boudouni', age: 33, department: 'IT', salary: 100000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 , specialization:'Javascript'},
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000,specialization:'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000,specialization:'Java' },
+      { id: 4, name: 'Nefeli Boudouni', age: 33, department: 'IT', salary: 100000 ,specialization:'Boss'},
 
     ];
 
@@ -29,3 +29,17 @@ function findEmployeeById(employeeId){
 
     }
 }
+function findEmployeeBySpecialization(specializationId){
+    const foundEmployee = employees.find(employee=>employee.specialization ==specializationId );
+    if (foundEmployee){
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name} - : ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}-${foundEmployee.specialization}</p>`;
+        }
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
+
+    }
+}
+function displayEmployees(){
+        const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} -${employee.specialization}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML = totalEmployees;
+  }
