8ff6372544db6c37615ec36f8b4f8a9c1a2545a9
DimNef <dimkrakis@gmail.com>
2024-10-22 17:39:03 -0400

    Promise API second task

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -18,4 +18,28 @@ function showweatherDetails(event){
                     weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
                   });
 }
+function showweatherlongDetails(event){
+    event.preventDefault();
+    const lon = document.getElementById('longitude').value;
+    const lat = document.getElementById('lattitude').value;
+    const apiKey = "7ff83f169c6356143621d669c9bd0d79";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    debugger;
+    fetch(apiUrl)
+                .then(response =>response.json())
+                .then(data=>{const weatherInfo = document.getElementById('weatherInfo');
+                weatherInfo.innerHTML =  `<h2>Weather in lon: ${data.coord.lon} and lat ${data.coord.lat}</h2>
+                <p>Temperature: ${data.main.temp} &#8451;</p>
+                <p>Weather: ${data.weather[0].description}</p>`
+
+            })
+            .catch(error => {
+                console.error('Error fetching weather:', error);
+                const weatherInfo = document.getElementById('weatherInfo');
+                weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+              });
+
+
+}
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+document.getElementById('weatherFormLon').addEventListener('submit',showweatherlongDetails );
